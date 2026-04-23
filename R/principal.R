8f75a1dd40c367d8cc005a96e9d99908aec22ed1
albertocarreto <alberto75carreto@gmail.com>
2026-04-23 14:20:22 -0600

    Update principal.R

--- R/principal.R
+++ R/principal.R
@@ -1,1 +1,16 @@
+# cargar funciones
+source("R/funciones.R")
+source("R/bootstrap.R")
 
+# cargar datos (ejemplo del libro de Foster et al. (2013)
+datos <- c(2,4,8,10)
+z <- 5
+
+# cálculo normal
+mg <- calcular_mg(datos)
+
+# cálculo bootstrap
+mg_boot <- bootstrap_mg(datos)
+
+print(mg)
+print(mg_boot)
